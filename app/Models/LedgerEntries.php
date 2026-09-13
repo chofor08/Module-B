41b8ab722a87f6d19605384e375267554fb115c6
GT <choforbarry008@gmail.com>
2026-09-13 22:21:42 +0100

    Set up Laravel project, design users, orders, orderitems,' purchasable items' and ledgerentries migrations

---
+++ app/Models/LedgerEntries.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class LedgerEntries extends Model
+{
+    //
+}
---
+++ database/factories/ItemsFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Items;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Items>
+ */
+class ItemsFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->word(),
+            'price' => fake()->randomFloat(2, 100, 1000),
+        ];
+    }
+}
